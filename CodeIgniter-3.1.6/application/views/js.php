034835d024a335523f1da51ba5aded257f451161
Logam <logam2@gmail.com>
2017-12-13 22:14:59 -0200

    problemas com a sitebar na mesma altura que a topo

--- CodeIgniter-3.1.6/application/views/js.php
+++ CodeIgniter-3.1.6/application/views/js.php
@@ -1,36 +1,36 @@
-<script src="assets/plugins/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo base_url('assets/plugins/jquery/dist/jquery.min.js') ?>"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="assets/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="<?php echo base_url('assets/plugins/jquery-ui/jquery-ui.min.js') ?>"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button);
 </script>
 <!-- Bootstrap 3.3.7 -->
-<script src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url('assets/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
 <!-- Morris.js charts -->
-<script src="assets/plugins/raphael/raphael.min.js"></script>
-<script src="assets/plugins/morris.js/morris.min.js"></script>
+<script src="<?php echo base_url('assets/plugins/raphael/raphael.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/plugins/morris.js/morris.min.js') ?>"></script>
 <!-- Sparkline -->
-<script src="assets/plugins/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<script src="<?php echo base_url('assets/plugins/jquery-sparkline/dist/jquery.sparkline.min.js') ?>"></script>
 <!-- jvectormap -->
-<script src="assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="<?php echo base_url('assets/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/plugins/jvectormap/jquery-jvectormap-world-mill-en.js') ?>"></script>
 <!-- jQuery Knob Chart -->
-<script src="assets/plugins/jquery-knob/dist/jquery.knob.min.js"></script>
+<script src="<?php echo base_url('assets/plugins/jquery-knob/dist/jquery.knob.min.js') ?>"></script>
 <!-- daterangepicker -->
-<script src="assets/plugins/moment/min/moment.min.js"></script>
-<script src="assets/plugins/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="<?php echo base_url('assets/plugins/moment/min/moment.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/plugins/bootstrap-daterangepicker/daterangepicker.js') ?>"></script>
 <!-- datepicker -->
-<script src="assets/plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="<?php echo base_url('assets/plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js') ?>"></script>
 <!-- Bootstrap WYSIHTML5 -->
-<script src="assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script src="<?php echo base_url('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js') ?>"></script>
 <!-- Slimscroll -->
-<script src="assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="<?php echo base_url('assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js') ?>"></script>
 <!-- FastClick -->
-<script src="assets/plugins/fastclick/lib/fastclick.js"></script>
+<script src="<?php echo base_url('assets/plugins/fastclick/lib/fastclick.js') ?>"></script>
 <!-- AdminLTE App -->
-<script src="assets/js/adminlte.min.js"></script>
+<script src="<?php echo base_url('assets/js/adminlte.min.js') ?>"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="assets/js/pages/dashboard.js"></script>
+<script src="<?php echo base_url('assets/js/pages/dashboard.js') ?>"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="assets/js/demo.js"></script>
+<script src="<?php echo base_url('assets/js/demo.js') ?>"></script>
--- CodeIgniter-3.1.6/application/views/side_bar2.php
+++ CodeIgniter-3.1.6/application/views/side_bar2.php
@@ -7,14 +7,14 @@
           <img src="<?php echo base_url('assets/img/user2-160x160.jpg') ?>" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
-          <p>Eduardo F. Nunes</p>
+          <p>Alexander Pierce</p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
       <!-- search form -->
       <form action="#" method="get" class="sidebar-form">
         <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <input type="text" name="q" class="form-control" placeholder="Procurar ...">
           <span class="input-group-btn">
                 <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
                 </button>
@@ -27,14 +27,14 @@
         <li class="header">MAIN NAVIGATION</li>
         <li class="active treeview">
           <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+            <i class="fa fa-dashboard"></i> <span>Cadastro</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+            <li class="active"><a href="<?=site_url('login')?>"><i class="fa fa-circle-o"></i> Listar Clientes</a></li>
+            <li><a href="index2.html"><i class="fa fa-circle-o"></i> Ordens de Serviço</a></li>
           </ul>
         </li>
         <li class="treeview">
@@ -196,4 +196,4 @@
       </ul>
     </section>
     <!-- /.sidebar -->
-  </aside>
+ </aside> 
